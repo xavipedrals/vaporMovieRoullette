c27d02cda6e38d923a1cde99b038a8d080c502d8
Xavi Pedrals <xavi.pedrals@gmail.com>
2020-10-28 09:53:56 +0100

    Updated with old Alamofire

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
         .package(name: "TelegramBotSDK", url: "https://github.com/zmeyc/telegram-bot-swift.git", from: "2.0.0"),
         .package(name: "Jobs", url: "https://github.com/BrettRToomey/Jobs.git", from: "1.1.1"),
-        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.2.0")),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "4.8.0"),
         .package(url: "https://github.com/JohnSundell/Files.git", from: "2.0.0"),
     ],
     targets: [
