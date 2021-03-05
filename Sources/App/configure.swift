b448aa4d2909a1df85d9540967eee67e36396395
Xavi Pedrals <xavi.pedrals@gmail.com>
2021-03-05 17:50:28 +0100

    Chnged time

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -21,7 +21,7 @@ public func configure(_ app: Application) throws {
     let dailyJob = DailyJobFuture()
     app.queues.schedule(dailyJob)
         .daily()
-        .at(16, 44)
+        .at(16, 52)
     
 //    let weeklyJob = WeeklyJob() {
 //        print("Weekly job finished")
