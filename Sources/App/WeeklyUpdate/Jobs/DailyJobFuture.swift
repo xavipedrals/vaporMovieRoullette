6c1c51cac39479cbfa7c049b5b67d82d97e2f778
Xavi Pedrals <xavi.pedrals@gmail.com>
2021-03-21 16:13:48 +0100

    fsdf

--- Sources/App/WeeklyUpdate/Jobs/DailyJobFuture.swift
+++ Sources/App/WeeklyUpdate/Jobs/DailyJobFuture.swift
@@ -19,38 +19,34 @@ class DailyJobFuture: ScheduledJob {
         databaseHelper = DatabaseHelper()
         databaseHelper.db = context.application.db
         eventLoop = context.eventLoop
-//        var operations = [EventLoopFuture<Void>]()
-//        for country in CountryCodes.all {
-//            let op = getAdditionsFuture(country: country).flatMap { () -> EventLoopFuture<Void> in
-//                self.getDeletionsFuture(country: country)
-//            }
-//            operations.append(op)
-//        }
-//        let allNetflixEvents = EventLoopFuture.andAllComplete(operations, on: eventLoop)
-//        let refreshDBJob = RefreshMaterializedViewsJob(databaseHelper: databaseHelper, eventLoop: eventLoop)
-//        let exportJob = ExportNetflixJob(databaseHelper: databaseHelper, eventLoop: eventLoop)
-//        let genreExportJob = GenreExportJob(eventLoop: eventLoop)
-        let findLostJob = FindLostJob(databaseHelper: databaseHelper, eventLoop: eventLoop)
+        var operations = [EventLoopFuture<Void>]()
+        for country in CountryCodes.all {
+            let op = getAdditionsFuture(country: country).flatMap { () -> EventLoopFuture<Void> in
+                self.getDeletionsFuture(country: country)
+            }
+            operations.append(op)
+        }
+        let allNetflixEvents = EventLoopFuture.andAllComplete(operations, on: eventLoop)
+        let refreshDBJob = RefreshMaterializedViewsJob(databaseHelper: databaseHelper, eventLoop: eventLoop)
+        let exportJob = ExportNetflixJob(databaseHelper: databaseHelper, eventLoop: eventLoop)
+        let genreExportJob = GenreExportJob(eventLoop: eventLoop)
+//        let findLostJob = FindLostJob(databaseHelper: databaseHelper, eventLoop: eventLoop)
         
-//        return allNetflixEvents
-//            .flatMap(getTmdbInfoFuture)
-//            .flatMap(getOmdbRatingsFuture)
-//            .flatMap {
-//                self.databaseHelper.removeDoubleQuotes(eventLoop: self.eventLoop)
-//            }
-//            .flatMap(refreshDBJob.run)
-//            .flatMap(exportJob.run)
-//            .flatMap(genreExportJob.run)
-        return findLostJob.run()
+        return allNetflixEvents
+            .flatMap(getTmdbInfoFuture)
+            .flatMap(getOmdbRatingsFuture)
+            .flatMap {
+                self.databaseHelper.removeDoubleQuotes(eventLoop: self.eventLoop)
+            }
+            .flatMap(refreshDBJob.run)
+            .flatMap(exportJob.run)
+            .flatMap(genreExportJob.run)
+//        return findLostJob.run()
 //        return getTmdbInfoFuture()
 //            .flatMap(getOmdbRatingsFuture)
 //            .flatMap(refreshDBJob.run)
 //            .flatMap(exportJob.run)
 //            .flatMap(genreExportJob.run)
-        
-//        return self.databaseHelper.removeDoubleQuotes(eventLoop: self.eventLoop)
-//            .flatMap(refreshDBJob.run)
-//            .flatMap(exportJob.run)
     }
     
     //MARK: - Private
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -21,7 +21,7 @@ public func configure(_ app: Application) throws {
     let dailyJob = DailyJobFuture()
     app.queues.schedule(dailyJob)
         .daily()
-        .at(11, 26)
+        .at(15, 15)
     
 //    let weeklyJob = WeeklyJob() {
 //        print("Weekly job finished")
