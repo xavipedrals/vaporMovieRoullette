ed9a1e58ae79b44981ec1262db18e64c7b468071
Xavi Pedrals <xavi.pedrals@gmail.com>
2021-06-10 12:21:57 +0200

    fdsfsd

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -21,7 +21,7 @@ public func configure(_ app: Application) throws {
     let dailyJob = DailyJobFuture()
     app.queues.schedule(dailyJob)
         .daily()
-        .at(10, 00)
+        .at(10, 26)
     
 //    let weeklyJob = WeeklyJob() {
 //        print("Weekly job finished")
